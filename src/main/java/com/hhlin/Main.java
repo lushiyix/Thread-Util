073b7022ad1afade8f8b0d7f624a1e6e8ed75732
lushiyix <1823068820@qq.com>
2023-10-30 17:05:56 +0800

    初始化项目

---
+++ src/main/java/com/hhlin/Main.java
@@ -0,0 +1,11 @@
+package com.hhlin;
+
+/**
+ * @author HuangHonglin
+ * @date 2023/10/30 17:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
